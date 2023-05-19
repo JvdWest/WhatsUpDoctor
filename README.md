d341469b58f704452e3aede0fa1c82d8cf6bea8a
JacquesVanDerWesthui <jacquesv@synthesis.co.za>
2023-05-19 16:45:50 +0200

    Added migration to create patient, practitioner and relationship tables, with auditable fields and effective date range on the linking table

--- README.md
+++ README.md
@@ -21,10 +21,20 @@ Create migration
 `python manage.py makemigrations`
 
 View SQL
-`python manage.py sqlmigrate WhatsUpDoctor MigrationFile`
+`python manage.py sqlmigrate app_name MigrationFile`
 
 Apply Migration
 `python manage.py migrate`
 
 Create Database User
 `python manage.py createsuperuser`
+
+Show migrations
+
+`python manage.py showmigrations`
+
+To undo a migration or to apply to a specific migration
+`python manage.py migrate app_name migration_name`
+
+Ignore unapplied migration
+`python manage.py migrate myapp migration_name --fake`
--- backoffice/models.py
+++ backoffice/models.py
@@ -1,17 +1,49 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class AuditableFields(models.Model):
+    create_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+        null=True,
+        related_name="%(class)s_created_by")
+    created_date = models.DateTimeField(auto_now_add=True)
+    last_modified_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+        null=True,
+        related_name="%(class)s_last_modified_by")
+    modified_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
 
 
-class Patient(models.Model):
+# To be applied to all linking tables
+class EffectiveRangeDates(models.Model):
+    effective_start_date = models.DateTimeField()
+    effective_end_date = models.DateTimeField()
+
+    class Meta:
+        abstract = True
+
+
+class Patient(AuditableFields):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     id_number = models.CharField(max_length=13)
-    created_date = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
 
-class Practitioner(models.Model):
+class Practitioner(AuditableFields):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    patients = models.ManyToManyField(Patient)
+    # Can be done with this, but requires auditable fields
+    # patients = models.ManyToManyField(Patient)
+
+
+class PractitionerPatient(AuditableFields, EffectiveRangeDates):
+    practitioner = models.ForeignKey(Practitioner, on_delete=models.PROTECT)  # Prevents accidental deletion
+    patient = models.ForeignKey(Patient, on_delete=models.PROTECT)
+
