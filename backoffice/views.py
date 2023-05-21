693bfc16ea4e9a0f1196ee4ee92fec33894eb83e
JacquesVanDerWesthui <jacquesv@synthesis.co.za>
2023-05-21 07:32:46 +0200

    Changed logic in API, if no query parameter was provided, the /patients API should return all active patients. Also added a filter on effective dates and is_deleted checks on all tables

--- backoffice/views.py
+++ backoffice/views.py
@@ -1,17 +1,26 @@
+import datetime
 import json
 
 from backoffice.models import Patient
 from django.http import JsonResponse
 from django.core import serializers
+from datetime import datetime
 
 
 def get_patients(request):
     hpcsa_number = request.GET.get('hpcsanumber')
 
+    current_date = datetime.utcnow().astimezone()
     if hpcsa_number is None:
-        return JsonResponse({'error': 'The hpcsanumber parameter is missing.'}, status=400)
-
-    queryset = Patient.objects.filter(practitionerpatient__practitioner__hpcsa_number__exact=hpcsa_number)
+        queryset = Patient.objects.filter(is_deleted=False)
+    else:
+        queryset = Patient.objects.filter(
+            practitionerpatient__practitioner__hpcsa_number__exact=hpcsa_number,
+            practitionerpatient__effective_start_date__lt=current_date,
+            practitionerpatient__effective_end_date__gt=current_date,
+            is_deleted=False,
+            practitionerpatient__is_deleted=False,
+            practitionerpatient__practitioner__is_deleted=False)
 
     serialized_data = serializers.serialize('json', queryset)
     json_data = json.loads(serialized_data)
